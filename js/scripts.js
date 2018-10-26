f2e8ddeadc33ac978cd13ba771e9a051b669367d
Katrina Hockman <khockman90@hotmail.com>
2018-10-26 15:44:00 -0700

    Add else if to allow number with 1 & 0 to be boop

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,11 @@ var beepBoop = function count(inputArray) {
 
     if (countUp[i].indexOf("1") > -1) {
        var resultNumbers = countUp.splice(countUp[i], 1, " Boop!");
-    }
 
+    } else if ((countUp[i].indexOf("0") > -1) && (countUp[i].indexOf("1") > -1)) {
+       var resultNumbers = countUp.splice(countUp[i], 1, " Boop!");
+    }
+    
     if (countUp[i].indexOf("0") > -1) {
        var resultNumbers = countUp.splice(countUp[i], 1, " Beep!");
     }
@@ -38,6 +41,10 @@ var beepBoop = function count(inputArray) {
     if (countUp[i] % 3 === 0) {
        var resultNumbers = countUp.splice(countUp[i], 1, " I'm sorry, Dave. I'm afraid I can't do that");
     }
+
+    // if ((countUp[i] % 3 !== 0) && (countUp[i].includes("0","1")) {
+    //   var resultNumbers = countUp.splice(countUp[i], 1, " Boop!");
+    // }
   }
  return countUp;
  };
